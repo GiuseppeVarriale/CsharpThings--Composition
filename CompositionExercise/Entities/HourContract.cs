1fd66c1095b491c3cedc94b97d09de0d640323bb
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-19 11:59:11 -0300

    Create HourContract Entity

---
+++ CompositionExercise/Entities/HourContract.cs
@@ -0,0 +1,25 @@
+﻿namespace CompositionExercise.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            this.ValuePerHour = valuePerHour;
+            this.Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
