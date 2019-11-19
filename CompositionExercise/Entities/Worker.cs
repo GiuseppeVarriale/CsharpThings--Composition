e7f90e1452c20ddd77743e541e93cbd37e539c5b
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-19 12:52:55 -0300

    Create Worker Entity

---
+++ CompositionExercise/Entities/Worker.cs
@@ -0,0 +1,12 @@
+﻿using CompositionExercise.Entities.Enums;
+
+namespace CompositionExercise.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+    }
+}
