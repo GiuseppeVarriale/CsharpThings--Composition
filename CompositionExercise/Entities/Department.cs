ee5daf7c203371b1e64718853f29aaed05360496
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-19 11:12:36 -0300

    Initial commit, Create Initial Department Entity

---
+++ CompositionExercise/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace CompositionExercise.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
